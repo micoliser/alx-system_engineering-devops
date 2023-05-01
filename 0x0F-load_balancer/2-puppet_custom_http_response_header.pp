7a0889738dafcdc897efaf19cd69c8b17130a377
micoliser <micoliser@gmail.com>
2023-05-01 14:46:22 +0100

    another fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,5 +1,5 @@
 # This puppet manifest installs and configures nginx server
 exec {'install':
+  provider => shell,
   command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=dQw4w9WgXcQ permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start ; sed -i "s/^\tlocation \/ {/\tlocation \/ {\n\t\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/sites-available/default ; service nginx restart',
-  path    => '/usr/bin'
 }
