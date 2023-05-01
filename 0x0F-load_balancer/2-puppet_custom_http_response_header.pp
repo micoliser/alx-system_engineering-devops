bf5042baaa76415a2bd9d3f3509eb4fe5c18e8a3
micoliser <micoliser@gmail.com>
2023-05-01 14:44:37 +0100

    another fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,45 +1,5 @@
 # This puppet manifest installs and configures nginx server
-exec {'update':
-  command => 'sudo apt-get -y update',
-  path    => '/usr/bin',
-}
-
-package {'nginx':
-  ensure   => 'installed',
-  name  => 'nginx',
-  provider => 'apt',
-}
-
-file {'default html':
-  ensure  => 'present',
-  path    => '/var/www/html/index.nginx-debian.html',
-  content => "Hello World!\n",
-}
-
-file {'404 error':
-  ensure  => 'present',
-  path    => '/usr/share/nginx/html/custom_404.html',
-  content => "Ceci n\'est pas une page\n",
-}
-
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => Package['nginx'],
-}
-
-exec {'configure':
-  command => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/micoliser permanent;\n\n\terror_page 404 \/custom_404.html;\n\tlocation = \/custom_404.html {\n\t\troot \/usr\/share\/nginx\/html;\n\t\tinternal;\n\t}/" /etc/nginx/sites-available/default',
-  path    => '/usr/bin',
-}
-
-exec { 'custom header':
-  command => "sudo sed -i \"s/^\tlocation \\\/ {/\\tlocation \\\/ {\\n\\t\\tadd_header X-Served-By '${::hostname}';/\" /etc/nginx/sites-available/default",
-  path    => '/usr/bin',
-}
-
-exec {'restart':
-  command     => 'sudo /usr/sbin/service nginx restart',
-  refreshonly => true,
-  subscribe   => Service['nginx'],
+exec {'install':
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=dQw4w9WgXcQ permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start ; sed -i "s/^\tlocation \/ {/\tlocation \/ {\n\t\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/sites-available/default ; service nginx restart',
+  path    => '/usr/bin'
 }
